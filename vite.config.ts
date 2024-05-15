4f2de1e91041e8665733b8eead8b512780ae709f
Nakaya <eniehack@outlook.jp>
2024-05-16 01:53:32 +0900

    feat: if dev's env, base path is root

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/gsk-archive-input-form",
+  base: process.env.NODE_ENV === "production" ? "/gsk-archive-input-form" : "/",
   plugins: [
     vue(),
   ],
