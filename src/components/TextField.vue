70312bcf7d0f52456488f0e4be0b8c4bbf50569e
Nakaya <eniehack@outlook.jp>
2024-05-16 02:30:25 +0900

    refactor: definePropsとdefineModelはコンパイラマクロなのでimportする必要がない

--- src/components/TextField.vue
+++ src/components/TextField.vue
@@ -9,8 +9,6 @@
 </template>
 
 <script setup lang="ts">
-    import { defineProps, defineModel } from "vue";
-
     const field = defineModel("field", { type: String, required: true });
 
     defineProps({
