d22654ab92073684b389f5bee2d9b08f445a5915
Nakaya <eniehack@outlook.jp>
2024-05-16 02:59:20 +0900

    refactor: わざわざPRODかどうかを確認してリテラル決め打ちしなくてもBASE_URLとかいうやつがあった

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.PROD ? "/gsk-archive-input-form" : "/"),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 
