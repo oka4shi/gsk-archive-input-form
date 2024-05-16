d55deaa59e0ca9dfa89f585b86b1cc13e4eab424
Nakaya <eniehack@outlook.jp>
2024-05-16 19:46:50 +0900

    feat: レビュー単体用入力フォーム

---
+++ src/routes/review/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+</script>
+
+<div class="text-sm breadcrumbs">
+	<ul>
+		<li><a href={`${base}/`}>げんしけん</a></li>
+		<li><a href={`${base}/review/`}>レビュー班</a></li>
+	</ul>
+</div>
+
+<div>
+	<ul>
+		<li><a href={`${base}/review/book`}>冊子</a></li>
+		<li><a href={`${base}/review/review`}>レビュー</a></li>
+	</ul>
+</div>
---
+++ src/routes/review/review/+page.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+	import TextField from '$lib/TextField.svelte';
+	import { base } from '$app/paths';
+	import { writable } from 'svelte/store';
+
+	type Review = {
+		title: string;
+		titleYomi: string;
+		subtitle: string;
+		subtitleYomi: string;
+		author: string;
+		startPage: string;
+		endPage: string;
+    includedIn: string;
+    note: string;
+	};
+
+	const initReview = (): Review => {
+		return {
+			title: '',
+		  titleYomi: "",
+		  subtitle: "",
+		  subtitleYomi: "",
+		  author: "",
+		  startPage: "",
+		  endPage: "",
+      includedIn: "",
+			note: ''
+		};
+	};
+
+	const review = writable(initReview());
+	let tsv: string;
+
+	review.subscribe((b) => {
+		tsv =
+			b.title +
+			'\t' +
+			b.titleYomi +
+			'\t' +
+			b.subtitle +
+			'\t' +
+			b.subtitleYomi +
+			'\t' +
+			b.author +
+			'\t' +
+			b.startPage
+			'\t' +
+			b.endPage +
+			'\t' +
+			b.includedIn +
+			'\t' +
+			b.note;
+	});
+
+	const copy = () => {
+		navigator.clipboard.writeText(tsv);
+	};
+</script>
+
+<div class="text-sm breadcrumbs">
+	<ul>
+		<li><a href={`${base}/`}>げんしけん</a></li>
+		<li><a href={`${base}/review/`}>レビュー班</a></li>
+		<li><a href={`${base}/review/book`}>冊子</a></li>
+	</ul>
+</div>
+<TextField label="タイトル" bind:value={$review.title} inputType="text" required />
+<TextField label="タイトルヨミ" bind:value={$review.titleYomi} inputType="text" required />
+<TextField label="サブタイトル" bind:value={$review.subtitle} inputType="text" required />
+<TextField label="サブタイトルヨミ" bind:value={$review.subtitleYomi} inputType="text" required />
+<TextField label="著者" bind:value={$review.author} inputType="text" required />
+<TextField label="開始ページ" bind:value={$review.startPage} inputType="number" required />
+<TextField label="終了ページ" bind:value={$review.endPage} inputType="number" required />
+<TextField label="備考" bind:value={$review.note} inputType="textarea" required />
+<button class="btn btn-primary" on:click={copy}>copy</button>
+<button class="btn btn-warning">clear</button>
